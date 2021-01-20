7a5a8487fe14d1b7a9db79b16f74532bc2382a95
Bruno Coviello Souto <bcovies@gmail.com>
2021-01-20 17:40:40 -0300

    UPDATE: main.sh [OK]

--- main.sh
+++ main.sh
@@ -37,17 +37,64 @@ configure_app_first_time(){
     read MAIN_APP_NAME
     echo "Projects HTTPS with '.git' statement in final"
     read GIT_HTTPS
-
 }
-confirm(){
 
+save(){
+    echo "Creating folders...."
+    if [[ -d $MAIN_FOLDER_DEPLOY_APP ]]; then
+        echo "Path $MAIN_FOLDER_DEPLOY_APP already exists"
+        echo "deleting..."
+        sudo rm -rf $MAIN_FOLDER_DEPLOY_APP
+    else
+        echo "Creating $MAIN_FOLDER_DEPLOY_APP"
+        sudo mkdir  -p $MAIN_FOLDER_DEPLOY_APP
+    fi
+
+    if [[ -d $MAIN_FOLDER_VOLUME_APP ]]; then
+        echo "Path $MAIN_FOLDER_VOLUME_APP already exists"
+        echo "deleting..."
+        sudo rm -rf $MAIN_FOLDER_VOLUME_APP
+    else
+        echo "Creating $MAIN_FOLDER_VOLUME_APP"
+        sudo mkdir  -p $MAIN_FOLDER_VOLUME_APP
+    fi
+    echo "saving it...."
+    if [[ -d $MAIN_FOLDER_DEPLOY_APP ]]; then
+        cd $MAIN_FOLDER_DEPLOY_APP
+        echo "MAIN_FOLDER_DEPLOY=$MAIN_FOLDER_DEPLOY" >> .env
+        echo "MAIN_FOLDER_VOLUME=$MAIN_FOLDER_VOLUME" >> .env
+        echo "MAIN_APP_NAME=$MAIN_APP_NAME" >> .env
+        echo "MAIN_FOLDER_DEPLOY_APP=$MAIN_FOLDER_DEPLOY_APP" >> .env
+        echo "MAIN_FOLDER_VOLUME_APP=$MAIN_FOLDER_VOLUME_APP" >> .env
+    else
+        echo "Error folder $MAIN_FOLDER_DEPLOY_APP does not exists"
+        exit 1
+    fi
+    
+}
+
+confirm(){
     echo $MAIN_APP_NAME
     echo $GIT_HTTPS
     MAIN_FOLDER_DEPLOY_APP=$MAIN_FOLDER_DEPLOY/$MAIN_APP_NAME
+    echo $MAIN_FOLDER_DEPLOY_APP
     MAIN_FOLDER_VOLUME_APP=$MAIN_FOLDER_VOLUME/$MAIN_APP_NAME
+    echo $MAIN_FOLDER_VOLUME_APP
+    echo "NOTE: THIS MIGHT DELETE FILES ON THIS PATH, BE SURE!" 
+    echo "Would you like to save this configuration? [y/n]"
+    read ANSWER
+    if [[ $ANSWER = 'y' ]]; then
+        save
+    else
+        echo "Discating all configs"
+        echo "Thanks for using!"
+        exit 1
+    fi
 }
+
 ask_config_new_app(){
-echo "Would you like to configure a app for the first?"
+echo "Would you like to configure a app for the first time?"
+echo "If there already is an app, it will be destroyed!"
 read ANSWER
 if [[ $ANSWER = 'n' ]]; then
     echo "Thanks for using!"
@@ -55,6 +102,7 @@ else
     configure_app_first_time
 fi
 }
+
 ###################################################
 #################    MAIN      ####################
 ###################################################
