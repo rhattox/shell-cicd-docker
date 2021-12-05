c5dcbd76b24c392e38c5506377a56d43b16b68fe
Bruno Souto <bcovies@gmail.com>
2021-12-05 05:00:11 -0300

    Adicionado status e logs

--- installer/logs.sh
+++ installer/logs.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
-# basic variables
+
 SCRIPT_COLLUMNS=$1
-# all services variables
 APP_NAME=$2
 
-entry_screen() {
-    echo -e "############################"
-    echo -e "######  SCRIPT  LOGS   #####"
-    echo -e "############################"
-}
-nodes_screen() {
-    for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
-    echo -e "\tStack Name ---> $APP_NAME"
-    for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
-    echo
-}
 stack_screen() {
-    for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
     docker node ls --format "{{.Hostname}}" | while read ENTRY_NODE; do
+        for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
+        echo "DOCKER NODE: $ENTRY_NODE"
+        for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
         docker -H $ENTRY_NODE ps -q | while read CONTAINER_ID; do
-            for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
-            echo "DOCKER NODE: $ENTRY_NODE"
+
             echo "Container ID: $CONTAINER_ID"
             echo "LOGS:"
             docker -H $ENTRY_NODE logs $CONTAINER_ID
-            for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
-            echo
         done
+        for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
+
     done
-    for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
-    echo
 }
 
-entry_screen
-nodes_screen
+# entry_screen
+# nodes_screen
 stack_screen
--- installer/status.sh
+++ installer/status.sh
@@ -20,14 +20,12 @@ nodes_screen() {
 stack_screen() {
     echo -e "\tStack Name ---> $APP_NAME"
     docker node ls --format "{{.Hostname}}" | while read ENTRY_NODE; do
-        for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
         echo "Node Name: ${ENTRY_NODE}"
-        for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
         OUTPUT_LINES=$(DOCKER_HOST=${ENTRY_NODE} docker ps -a --no-trunc -f name=$APP_NAME | wc -l)
         if [[ $OUTPUT_LINES != 1 ]]; then
-            for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "^v"; done
+            for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
             DOCKER_HOST=${ENTRY_NODE} docker ps -a --format "$(tput setaf 4)\nImage:\t{{.Image}}\nID:\t{{.ID}}\nName:\t{{.Names}}\nStatus:\t{{.Status}}\nPorts:\t{{.Ports}}\nState:\t{{.State}}\nNetworks:\t{{.Networks}}\nMounts:\t{{.Mounts}}\nLabels:\t{{.Labels}}\nSize:\t{{.Size}}\nRunningFor:\t{{.RunningFor}}\nCreatedAt:\t{{.CreatedAt}}\nCommand:\t{{.Command}}\nMount:\t{{.Mounts}}\nRunning:\t{{.RunningFor}}$(tput sgr0)" --no-trunc -f name=$APP_NAME
-            for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "^v"; done
+            for ((i = 0; i < $SCRIPT_COLLUMNS; i++)); do printf "="; done
         fi
     done
 }
