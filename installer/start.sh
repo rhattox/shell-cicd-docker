6aa7248c0d7b49dee602050f4058ef48b8edc19b
Bruno Souto <bcovies@gmail.com>
2022-02-01 21:16:32 -0300

    Adicionado cabecalho

--- installer/start.sh
+++ installer/start.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+#
+# start.sh - Faz start do serviço docker pelo arquivo docker-swarm.yml
+#
+# Autor: Bruno Souto <bcovies@gmail.com>
+#
+# -----------------------------------------------------------------------------------------------------------
+#       Este programa executa o comando de start na pasta que está localizado o arquivo docker-compose.yml
+#   e necessita de um uma imagem docker que é responsável por redirecionar o entrypoint para este script.
+# -----------------------------------------------------------------------------------------------------------
+#
+#   Histórico:
+#
+#   v0.0.1 - 2021-12-05
+#       - criado script básico para start do serviço do swarm
+#
+#
+#   Licença: GPL.
 
-# basic variables
+
 SCRIPT_COLLUMNS=$1
-# all services variables
 APP_NAME=$2
 DOCKER_APP_FULL_PATH=$3
 
@@ -21,6 +37,11 @@ start_stack() {
     echo
 }
 
-entry_screen
 
-start_stack
+init(){
+    entry_screen
+    start_stack
+}
+
+init
+
